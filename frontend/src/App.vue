dfc6ab53523590b3a4a6bb7f1d91e10346829d3f
sackey21 <sackey.sg@gmail.com>
2025-03-20 20:15:43 +0900

    諸々共通で出せるように

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,20 @@
+<!-- viewsのそれぞれのページにアクセスできる -->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
 <script setup lang="ts">
-import VideoView from './views/VideoView.vue'
 </script>
 
-<template>
-  <main>
-    <VideoView />
-  </main>
-</template>
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
