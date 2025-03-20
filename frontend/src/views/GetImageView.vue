35ed25d5c19a485ab4cfd6b5a2d82c123ce5292f
sackey21 <sackey.sg@gmail.com>
2025-03-20 11:49:36 +0900

    最低限の画像保存機構完成

---
+++ frontend/src/views/GetImageView.vue
@@ -0,0 +1,23 @@
+<!-- 画像保存するのスマホ画面 -->
+<template>
+  <div>
+    <h1>画像を保存する</h1>
+    <div>
+      <img :src="image" alt="image" />
+    </div>
+    <button @click="saveImage">保存</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import img from './../../captureImage/capture.png'
+
+const image = ref<string>(img)
+const saveImage = async () => {
+    const a = document.createElement('a')
+    a.href = image.value
+    a.download = 'captureImage/capture.png'
+    a.click()
+}
+</script>
