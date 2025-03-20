0e17dd2358d1eaf6ffe61e6e02753f233b0a291b
sackey21 <sackey.sg@gmail.com>
2025-03-20 15:36:44 +0900

    画像の表示とダウンロード

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import GetImageView from '../views/GetImageView.vue'
+import HomeView from '@/views/HomeView.vue'
+import GetImageView from '@/views/GetImageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- frontend/src/views/GetImageView.vue
+++ frontend/src/views/GetImageView.vue
@@ -1,4 +1,3 @@
-<!-- 画像保存するのスマホ画面 -->
 <template>
   <div>
     <h1>画像を保存する</h1>
@@ -10,14 +9,39 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import img from './../../captureImage/capture.png'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
-const image = ref<string>(img)
+const image = ref<string>('')
+
+// 画像をデバイスにダウンロードさせる
 const saveImage = async () => {
+  // imageの値が空の場合は処理を中断
+  if (!image.value){
+    console.error('画像がありません')
+    return
+  } 
+  return new Promise((resolve, reject) => {
     const a = document.createElement('a')
     a.href = image.value
-    a.download = 'captureImage/capture.png'
+    a.download = 'image.png'
     a.click()
+    resolve('画像保存成功')
+  })
 }
+
+const getImage = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/get_image/')
+    image.value = `data:image/png;base64,${response.data.image}`
+    console.log('画像取得成功')
+  } catch (error) {
+    console.error('画像取得エラー: ', error)
+  }
+}
+
+// ページが読み込まれたときにgetImage関数を実行
+onMounted(() => {
+  getImage()
+})
 </script>
